7c0c79f1dbcfff734e0678cb829e3491103cb97d
KiligWYu <hnlywangyu@gmail.com>
2019-01-11 11:05:52 +0800

    #27

---
+++ #27.swift
@@ -0,0 +1,11 @@
+//
+//  #27
+//  https://leetcode.com/problems/remove-element/
+//
+
+class Solution {
+    func removeElement(_ nums: inout [Int], _ val: Int) -> Int {
+        nums = nums.filter { $0 != val }
+        return nums.count
+    }
+}
