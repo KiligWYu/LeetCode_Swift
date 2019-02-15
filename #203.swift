993a1039a81bd7048f5dee625690fb33ae7ebbef
KiligWYu <hnlywangyu@gmail.com>
2019-02-15 17:23:42 +0800

    #203

---
+++ #203.swift
@@ -0,0 +1,24 @@
+//
+//  #203
+//  https://leetcode.com/problems/remove-linked-list-elements/
+//
+
+class Solution {
+    func removeElements(_ head: ListNode?, _ val: Int) -> ListNode? {
+        var newHead = head
+        while newHead?.val == val {
+            newHead = newHead?.next
+        }
+        
+        var curr = newHead
+        while curr?.next != nil {
+            if curr?.next?.val == val {
+                curr?.next = curr?.next?.next
+            } else {
+                curr = curr?.next
+            }
+        }
+        
+        return newHead
+    }
+}
