1b3c37e5d067eee57208969902c0d7f6c5f27634
KiligWYu <hnlywangyu@gmail.com>
2019-02-21 15:47:25 +0800

    #242

---
+++ #242.swift
@@ -0,0 +1,14 @@
+//
+//  #242
+//  https://leetcode.com/problems/valid-anagram/
+//
+
+class Solution {
+    func isAnagram(_ s: String, _ t: String) -> Bool{
+        guard s.count == t.count else {
+            return false
+        }
+        
+        return s.sorted() == t.sorted()
+    }
+}
